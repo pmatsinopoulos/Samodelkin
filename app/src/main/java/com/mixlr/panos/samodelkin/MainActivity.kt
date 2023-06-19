4f00a0f876ef019d2d474c194085e68cef717717
Panos Matsinopoulos <panos@mixlr.com>
2023-06-19 13:49:24 +0300

    Display data from the character generator

--- app/src/main/java/com/mixlr/panos/samodelkin/MainActivity.kt
+++ app/src/main/java/com/mixlr/panos/samodelkin/MainActivity.kt
@@ -2,10 +2,25 @@ package com.mixlr.panos.samodelkin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mixlr.panos.samodelkin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var characterData = CharacterGenerator.generate()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        characterData.run {
+            val cd = this
+            binding.apply {
+                nameTextView.text = cd.name
+                raceTextView.text = cd.race
+                dexterityTextView.text = cd.dex
+                wisdomTextView.text = cd.wis
+                strengthTextView.text = cd.str
+            }
+        }
     }
 }
